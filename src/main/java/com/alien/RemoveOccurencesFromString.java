5e235dc238fac976dd8b750456a43aec39141d2a
Chetan Malabade <malabade.chetan@gmail.com>
2024-06-28 20:15:15 +0530

    remove occurences

---
+++ src/main/java/com/alien/RemoveOccurencesFromString.java
@@ -0,0 +1,14 @@
+package com.alien;
+
+//  How do you remove all occurrences of a given character from the input string?
+
+public class RemoveOccurencesFromString {
+
+	public static void main(String[] args) {
+
+		String str = "javaalien";
+		str = str.replace("a", "");
+		System.out.println(str);
+
+	}
+}
