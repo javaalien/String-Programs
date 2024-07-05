f1b7dce4b2f63b3d8567b2dfb6a1e9db149cf27b
Chetan Malabade <malabade.chetan@gmail.com>
2024-07-05 19:17:55 +0530

    good string

--- src/main/java/com/alien/GoodString_AreOccurrencesEqual.java
+++ src/main/java/com/alien/GoodString_AreOccurrencesEqual.java
@@ -2,7 +2,7 @@ package com.alien;
 
 // Given a string s, return true if s is a "good" string, or false otherwise. 
 // A string s is good if all characters that appear 
-// in s have the same number of occurrences (i.e., the same frequency).
+// in s have the same number of occurrences (i.e., the same frequency)..
 
 public class GoodString_AreOccurrencesEqual {
 
